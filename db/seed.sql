da909fe2a9b6156cef7e153579d11b76ad887549
karltun Moreno <karltunmoreno@gmail.com>
2022-03-23 19:18:07 -0400

    work

---
+++ db/seed.sql
@@ -0,0 +1,24 @@
+INSERT INTO Departments (department_name, department_id)
+VALUES
+('Finance', 1),
+('Engineering', 2),
+('Sales', 3),
+('Human Resources', 4);
+
+INSERT INTO Roles (job_title, role_id, salary)
+VALUES
+('Accountant', 0771, 999000),
+('Recruiter', 0055, 0101010),
+('Software Engineer', 4545, 45445455),
+('General Manager', 67677, 67677676),
+('Business Analyst', 1211, 2122121),
+('Digital Marketing', 09099, 09090909);
+
+INSERT INTO Employees (first_name, last_name, job_title, employee_salary)
+VALUES
+('Jonathan', 'Chow', 'Business Analyst', 5000),
+('Manuel', 'Rodriguez', 'Customer Service', 81000),
+('Andrea', 'Jackson', 'Social Media', 61000),
+('Joshua', 'Thayer', 'Project Manager', 31000),
+('Christina', 'Smith', 'Sales Representative', 210000),
+('Gabriel', 'Cannon', 'Web Designer', 560000);
